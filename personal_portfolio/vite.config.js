cd2401efba22a048f1cc63fe01bf5ebb7f5a632a
shahd mohamed <159973320+shahed137003@users.noreply.github.com>
2025-08-08 03:49:59 +0300

    try to fix

--- personal_portfolio/vite.config.js
+++ personal_portfolio/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: process.env.VITE_BASE_PATH || "/personal-portfolio", 
+  plugins: [react(), tailwindcss()],
+  base: "/"
 })
